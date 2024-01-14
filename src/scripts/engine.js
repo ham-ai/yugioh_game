9f3d523cda2aa79322e4ba4e46152a5b90afb510
abraham <abrahamluccas13@gmail.com>
2024-01-14 00:19:26 -0300

    functions 'n objects

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -12,12 +12,61 @@ const state = {
     fieldCards:{
         player: document.getElementById("player-field-card"),
         computer: document.getElementById("computer-field-card")
+    },
+    actions:{
+        button:document.getElementById("next-duel"),
+    }
+};
+
+const playerSides = {
+    player1: "player-field-card",
+    computer: "computer-field-card"
+}
+
+const pathImages = ".src/assets/icons/";
+
+const cardData = [
+    {
+        id:0,
+        name: "Blue Eyes White Dragon",
+        type: "Paper",
+        img: `${pathImages}dragon.png`,
+        winOf:[1],
+        loseOf:[2]
+    },
+    {
+        id:1,
+        name: "Dark Magician",
+        type: "Rock",
+        img: `${pathImages}magician.png`,
+        winOf:[2],
+        loseOf:[0]
+    },
+    {
+        id:2,
+        name: "Exodia",
+        type: "Scissor",
+        img: `${pathImages}exodia.png`,
+        winOf:[0],
+        loseOf:[1]
+    },
+]
+
+// functions
+
+async function drawCards(cardNumbers, fieldSide){
+    for (let i = 0; i < cardNumbers.length; i++) {
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
+
+        document.getElementById(fieldside).appendChild(cardImage);
     }
 }
 
 
 function init(){
-
+    drawCards(5, playerSides.player1);
+    drawCards(5, playerSides.computer);
 }
 
 inti();
